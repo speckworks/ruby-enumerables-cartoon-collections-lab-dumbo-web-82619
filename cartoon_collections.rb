e0260486d3de43775a0422a82a11c07338aff7b2
John Karrell Speck <speckworks@gmail.com>
2019-08-18 16:19:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def long_planeteer_calls(calls_long)
 end
 
 
-def find_the_cheese(contains_cheddar, h = nil) 
+def find_the_cheese(contains_cheddar) 
       cheese_types = ["cheddar", "gouda", "camembert"]
       # cheese_types.find do |h|  if h == contains_cheddar 
       # end
