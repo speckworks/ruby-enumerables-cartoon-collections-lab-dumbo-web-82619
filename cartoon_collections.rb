cb9ec3bc07af40cd9ec5b4b32c666d1943f7c08d
John Karrell Speck <speckworks@gmail.com>
2019-08-18 15:13:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,54 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|dwarf, index| 
+    puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  veggies.collect do |word|
+  veggies = word.split('')
+  veggies.join
+  ("#{word.capitalize}!")
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+# it "returns true if any calls are longer than 4 characters" do
+#       calls_long = ["axe", "earth", "wind", "fire"]
+#       expect(long_planeteer_calls(calls_long)).to eq(true)
+#     end
+def long_planeteer_calls(calls_long)
+  calls_long.any? do |a| a.length > 4 
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+
+def find_the_cheese(contains_cheddar, h = nil) 
+      cheese_types = ["cheddar", "gouda", "camembert"]
+      cheese_types.find do |h|  if h == contains_cheddar 
+      end
+      puts "#{h}"
+  end
+  
+    
+    
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
+
+#   describe "#find_the_cheese" do
+#     it "returns the first element of the array that is cheese" do
+#       contains_cheddar = ["banana", "cheddar", "sock"]
+#       expect(find_the_cheese(contains_cheddar)).to eq 'cheddar'
+
+#       contains_gouda = ["potato", "gouda", "camembert"]
+#       expect(find_the_cheese(contains_gouda)).to eq 'gouda'
+#     end
+
+#     it "returns nil if the array does not contain a type of cheese" do
+#       no_cheese = ["ham", "cellphone", "computer"]
+#       expect(find_the_cheese(no_cheese)).to eq nil
+#     end
+#   end
+# end
